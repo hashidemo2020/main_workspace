6cd83853e33a7a0cf17a6acbb85313448201f1af
Prakash Manglanathan <pmnathan@gmail.com>
2020-01-22 11:08:37 -0500

    update to module version

--- 4-ec2-instance-module.tf
+++ 4-ec2-instance-module.tf
@@ -1,6 +1,6 @@
 module "ec2-instance" {
   source  = "app.terraform.io/fidelity_tfe_demo/ec2-instance/aws"
-  version = "2.12.0"
+  version = "2.15.0"
 
   name                   = var.project_name
   instance_count         = 1
